809859cd9d794cddc1fd7426a5d3dad2ae5dd751
Yago Russo Cabral <yagrrusso@gmail.com>
2022-06-10 13:26:43 -0300

    postgres

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -4,9 +4,12 @@ import * as ApiController from '../controllers/apiController'
 
 const router = Router()
 
-router.get('/', ApiController.getTasks)
-router.post('/new', ApiController.createTask)
-router.put('/update/:id', ApiController.updateTask)
-router.delete('/delete/:id', ApiController.deleteTask)
+// router.get('/', ApiController.getTasks)
+// router.post('/new', ApiController.createTask)
+// router.put('/update/:id', ApiController.updateTask)
+// router.delete('/delete/:id', ApiController.deleteTask)
+router.get('/', (req, res) => {
+   res.json({ working: true })
+})
 
 export default router
